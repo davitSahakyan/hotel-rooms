483e27fa4fc3d764c8d6d9454e77ee29d26b4b0b
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-02-18 15:00:14 +0400

    Add : add navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
-
+import logo from '../images/logo.png';
+import { FaAlignRight } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+ 
 export default class Navbar extends Component {
+  
+  state={
+    isOpen : false,
+  }
+
+  handleToggle = () =>{
+    const { isOpen } = this.state
+    this.setState({ isOpen : !isOpen})
+  }
+
   render() {
+    const { isOpen } = this.state
     return (
-      <div>
-        Hello from Navbar
-      </div>
+      <nav className='navbar'>
+        <div className='nav-center'>
+          <div className='nav-header'>
+            <Link to='/'>
+              <img src={logo} alt='hotelImage' width='300px' />
+            </Link>
+            <button 
+              type='button' 
+              className='nav-btn'
+              onClick={this.handleToggle}
+              >
+              <FaAlignRight className='nav-icon' />
+            </button>
+          </div>
+          <ul className={ isOpen ? "nav-links show-nav" : "nav-links"}>
+             <li>
+               <Link to='/'> home </Link>
+             </li>
+             <li>
+               <Link to='/rooms'> rooms </Link>
+             </li>
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
