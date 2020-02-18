acbf8e9773873bfde1a2d26752387b04d1c58e63
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-02-18 13:56:24 +0400

    Add : routes for components in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
+// Components
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Error from './pages/Error'
+import Rooms from './pages/Rooms'
+import SingleRoom from './pages/SingleRoom'
+// Router
+import { Switch , Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <>
+     <Navbar />
+     <Switch>
+       <Route path='/' component={Home} exact />
+       <Route path='/rooms/' component={Rooms} exact />
+       <Route path='/rooms/:slug' component={SingleRoom} exact />
+       <Route  component={Error}  />
+     </Switch>
+    </>
   );
 }
 
