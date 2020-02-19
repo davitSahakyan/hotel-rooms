6dbb0e2e306ff57d7a6f9a67c765f10c74eb3a6b
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-02-19 23:10:42 +0400

    Style : add style to banner component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
-     <Hero />
+     <Hero>
+        <Banner 
+           title='luxurious rooms' 
+           subtitle='delux rooms starting at 199$'
+           >
+          <Link to='/rooms' className='btn-primary'> our rooms </Link>
+        </Banner>
+     </Hero>
   );
 }
