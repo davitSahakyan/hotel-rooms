33ab7f1b83d5fa69530d876f9dc099403ffc8486
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-03-28 21:54:28 +0400

    Add functions handleChange and filterRooms in context

--- src/context.js
+++ src/context.js
@@ -52,10 +52,25 @@ class RoomProvider extends React.Component {
         return room;
     };
 
+    handleChange = event => {
+        const type = event.target.type;
+        const name = event.target.name;
+        const value = event.target.value;
+        console.log(type, name, value);
+    };
+
+    filterRooms = () => {
+        console.log("hello");
+    };
+
     render() {
         return (
             <RoomContext.Provider
-                value={{ ...this.state, getRoom: this.getRoom }}
+                value={{
+                    ...this.state,
+                    getRoom: this.getRoom,
+                    handleChange: this.handleChange
+                }}
             >
                 {this.props.children}
             </RoomContext.Provider>
