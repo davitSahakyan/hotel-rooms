d6db5d9f3b07361f3fda938b5e285c7cf230ee0e
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-04-01 23:25:25 +0400

    Fix capacity is number and filter by guests works

--- src/context.js
+++ src/context.js
@@ -79,10 +79,17 @@ class RoomProvider extends React.Component {
             breakfast,
             pets
         } = this.state;
-
+        // all rooms
         let tempRooms = [...rooms];
+        // transform values
+        capacity = parseInt(capacity);
+        // filter by type
         if (type !== "all") {
             tempRooms = tempRooms.filter(room => room.type === type);
+        }
+        // filter by capacity
+        if (capacity !== 1) {
+            tempRooms = tempRooms.filter(room => room.capacity >= capacity);
         }
         this.setState({
             sortedRooms: tempRooms
