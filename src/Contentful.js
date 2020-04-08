7989ac58725654da2999ec008a1edee70e28dc79
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-04-08 22:42:55 +0400

    Add .env for contentfull accessToken safety

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,6 @@
 import { createClient } from "contentful";
 
 export default createClient({
-    space: "vr7w1bga0v8g",
-    accessToken: "YdtBi6qzQcPoXY5XAb4bB9SsYy77NAc_EvM64r2yZVc",
+    space: process.env.REACT_APP_API_SPACE,
+    accessToken: process.env.REACT_APP_ACCESSTOKEN,
 });
